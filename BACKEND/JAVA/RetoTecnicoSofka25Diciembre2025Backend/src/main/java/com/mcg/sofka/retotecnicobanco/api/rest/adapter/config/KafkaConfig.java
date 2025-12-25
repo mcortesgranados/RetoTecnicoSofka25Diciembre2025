12cbd5feacb91f6728aff30f78e2efaf0b7227a5
Manuela Cortés Granados <manuelacortesgranados@gmail.com>
2025-12-25 08:22:16 -0500

    chore: add Kafka configuration class placeholder
    - Added `KafkaConfig` class with `@Configuration`.
    - Placeholder for defining Kafka connection beans.

---
+++ BACKEND/JAVA/RetoTecnicoSofka25Diciembre2025Backend/src/main/java/com/mcg/sofka/retotecnicobanco/api/rest/adapter/config/KafkaConfig.java
@@ -0,0 +1,8 @@
+package com.mcg.sofka.retotecnicobanco.api.rest.adapter.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+    // Define Kafka connection beans
+}
