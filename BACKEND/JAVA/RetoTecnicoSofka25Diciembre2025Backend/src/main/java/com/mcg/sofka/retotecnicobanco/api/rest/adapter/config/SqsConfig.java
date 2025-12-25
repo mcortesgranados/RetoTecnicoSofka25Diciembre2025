432495fb2716f4b111e593228917554fdc6c9e27
Manuela Cortés Granados <manuelacortesgranados@gmail.com>
2025-12-25 08:22:52 -0500

    chore: add SQS configuration class placeholder
    - Added `SqsConfig` class with `@Configuration`.
    - Placeholder for defining AWS SQS connection beans.

---
+++ BACKEND/JAVA/RetoTecnicoSofka25Diciembre2025Backend/src/main/java/com/mcg/sofka/retotecnicobanco/api/rest/adapter/config/SqsConfig.java
@@ -0,0 +1,8 @@
+package com.mcg.sofka.retotecnicobanco.api.rest.adapter.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SqsConfig {
+    // Define SQS connection beans
+}
