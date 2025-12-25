c5013b52019cce5b7332cb4084b11af0aef2ac28
Manuela Cortés Granados <manuelacortesgranados@gmail.com>
2025-12-25 06:47:34 -0500

    feat(app): add main Spring Boot application class for Reto Tecnico Sofka
    - Introduced `RetoTecnicoSofka25Diciembre2025Application` as the entry point of the Spring Boot app.
    - Annotated with `@SpringBootApplication` to enable component scanning, auto-configuration, and
      Spring Boot features.
    - Includes Javadoc with author, timestamp, and context for the Reto Tecnico Sofka 25 Diciembre 2025.
    - Main method invokes `SpringApplication.run` to start the application.

---
+++ BACKEND/JAVA/RetoTecnicoSofka25Diciembre2025Backend/src/main/java/com/mcg/sofka/retotecnicobanco/RetoTecnicoSofka25Diciembre2025Application.java
@@ -0,0 +1,21 @@
+package com.mcg.sofka.retotecnicobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Reto Tecnico Sofka 25 Diciembre 2025 Application Main Class
+ *
+ * @author Manuela Cortés Granados (manuelacortesgranados@gmail.com)
+ * @since 25 Diciembre 2025 06:50 AM GMT -5 Bogotá DC Colombia
+ * 
+ */
+
+@SpringBootApplication
+public class RetoTecnicoSofka25Diciembre2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoTecnicoSofka25Diciembre2025Application.class, args);
+	}
+
+}
