8b57ac140e887a600882460ed128dd59f889a598
Manuela Cortés Granados <manuelacortesgranados@gmail.com>
2025-12-25 08:28:04 -0500

    chore: add health check REST controller
    - Created `HealthCheckController` with `/health` endpoint.
    - Returns HTTP 200 with message "springboot-lambda up".

---
+++ BACKEND/JAVA/RetoTecnicoSofka25Diciembre2025Backend/src/main/java/com/mcg/sofka/retotecnicobanco/api/rest/adapter/rest/command/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.mcg.sofka.retotecnicobanco.api.rest.adapter.rest.command;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> check() {
+        return ResponseEntity.ok("springboot-lambda up");
+    }
+}
